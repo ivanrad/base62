e19d34ae07020b81498298ca6308083f29792378
ivanrad <ivanra@gmail.com>
2022-03-15 18:10:57 +0000

    use t.Helper() to mark fuzz function

--- fuzz_test.go
+++ fuzz_test.go
@@ -10,16 +10,17 @@ import (
 
 func FuzzEncodeDecode(f *testing.F) {
 	for _, seed := range [][]byte{
-		[]byte{},
-		[]byte{0},
-		[]byte{0xff, 0xfe},
-		[]byte{0, 1, 2, 3, 4, 5},
+		{},
+		{0},
+		{0xff, 0xfe},
+		{0, 1, 2, 3, 4, 5},
 		[]byte("\x53\xfe\x92\xfe\xff\x00\xab"),
 	} {
 		f.Add(seed)
 	}
 
 	f.Fuzz(func(t *testing.T, b []byte) {
+		t.Helper()
 		encoded := EncodeToString(b)
 		decoded, err := DecodeString(encoded)
 		if err != nil {
